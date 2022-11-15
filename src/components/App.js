b50358a4cedbe8d7596c9e1eacbd982e06bdcfe7
Tim Empey <timothyempey@gmail.com>
2022-11-15 09:27:55 -0800

    add state + buttons
    Co-authored-by: SpencerJDennis <SpencerJDennis@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./Header";
-import TicketList from "./TicketList";
+import TicketControl from "./TicketControl";
 
 function App(){
-  const name = "Thato";
-  const name2 = "Haley";
   return (
     <React.Fragment>
       <Header />
-      <TicketList />
+      <TicketControl />
     </React.Fragment>
   );
 }
---
+++ src/components/TicketControl.js
@@ -0,0 +1,39 @@
+import React from "react";
+import NewTicketForm from "./NewTicketForm";
+import TicketList from "./TicketList";
+
+class TicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm />
+      buttonText = "Return to Ticket List";
+    } else {
+      currentlyVisibleState = <TicketList />
+      buttonText = "Add Ticket";
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default TicketControl;
